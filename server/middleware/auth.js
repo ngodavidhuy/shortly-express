a56238b1e46caa2eeac0edade9a8691c11ffddcb
ngodavidhuy <ngodavidhuy@gmail.com>
2018-12-07 19:36:10 -0800

    Implements cookieParser!

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -2,16 +2,26 @@ const models = require('../models');
 const Promise = require('bluebird');
 
 module.exports.createSession = (req, res, next) => {
-  models.Sessions.create()
-    .then(session => {
-      //console.log(session);
-      return models.Sessions.get({id: session.insertId});
-    })
-    .then(sessionRow => {
-
-    });
+  // if (req.cookies.hasOwnProperty('sessId')) {
+  //   console.log(req.body, 'checking on cookies');
+  //   models.Sessions.get({id: req.c});
+  //   next();
+  // } else {
+  //   models.Sessions.create()
+  //     .then(session => {
+  
+  //       //console.log(session);
+  //       return models.Sessions.get({id: session.insertId});
+  //     })
+  //     .then(sessionRow => {
+  //       console.log(sessionRow);
+  //       res.cookie('sessId', sessionRow.hash);
+  //       res.send();
+  //     });
+      
+  //   }
   next();
-};
+  };
 
 /************************************************************/
 // Add additional authentication middleware functions below
--- server/middleware/cookieParser.js
+++ server/middleware/cookieParser.js
@@ -1,4 +1,12 @@
 const parseCookies = (req, res, next) => {
+  req.cookies = {};
+  if (req.headers.cookie !== undefined) {
+    let cookies = req.headers.cookie.split('; ');
+    for (let cookie of cookies) {
+      let pair = cookie.split('=');
+      req.cookies[pair[0]] = pair[1];
+    }
+  }
   next();
 };
 
